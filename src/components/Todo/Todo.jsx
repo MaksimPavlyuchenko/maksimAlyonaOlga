39cd291c586e5d2ac2ace3c5433f4a42ac26d7f1
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-07-23 12:49:26 +0300

    task-two-maks

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,13 +2,18 @@ import { RiDeleteBinLine } from 'react-icons/ri';
 import { Text } from 'components';
 import { DeleteButton, TodoWrapper } from './Todo.styled';
 import { useDispatch } from 'react-redux';
-import { deleteTodos } from 'redux/todoSlice';
+import { deleteTodos, updateTodo } from 'redux/todoSlice';
+import { useState } from 'react';
 
 export const Todo = ({ text, counter, id }) => {
+  const [updateText, setUpdateText] = useState(text);
   const dispatch = useDispatch();
 
   const deleteTodo = () => {
     dispatch(deleteTodos(id));
+  };
+  const handleUpdate = () => {
+    dispatch(updateTodo({ id, text: updateText }));
   };
   return (
     <>
@@ -20,6 +25,16 @@ export const Todo = ({ text, counter, id }) => {
         <DeleteButton type="button" onClick={deleteTodo}>
           <RiDeleteBinLine size={24} />
         </DeleteButton>
+        <input
+          type="text"
+          value={updateText}
+          onChange={e => {
+            setUpdateText(e.target.value);
+          }}
+        />
+        <button type="button" onClick={handleUpdate}>
+          UPDATE
+        </button>
       </TodoWrapper>
     </>
   );
